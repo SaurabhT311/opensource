7b97c341d08df3474053db334ee41757e321c13f
SaurabhT311 <38470650+SaurabhT311@users.noreply.github.com>
2020-04-11 20:58:43 +0530

    Update saurabh.php

--- saurabh.php
+++ saurabh.php
@@ -1,61 +1,60 @@
-<!DOCTYPE html>
+
+<html lang="en">
     <head>
     <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
-  <title>Rental Portal</title>
+  <title>Renting Portal</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="design.css"/>
+  <title>Flat Search Portal</title>
 </head>
-<style>
-body
-    {
-        background-image:url("bulb2.jpg");
-        background-size: 1420px 720px;
-        background-repeat: no-repeat;
-        font-size: 140%;
-    }
-    h1 {
-            text-shadow: 0 0 3px #FF0000;
-        }
-    
-</style>
-<style>
-div {
-  background-image: url('bulb2.jpg');
-}
-</style>
-
-
 <body>
     <center>
-<h1 style="color:maroon;"><center><marquee><b>Welcome to Flatbricks</b></center></marquee></h1>
 
-<br><br><br>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
 
+<div class="wrapper">
+  <div class="title">
+    <h2><b>Welcome to Flatbricks</b></h2>
+  </div>
+  <div class="social_media">
+    <div class="item">
+      <i class="fab fa-facebook-f"></i>
+    </div>
+    <div class="item">
+      <i class="fab fa-twitter"></i>
+    </div>
+    <div class="item">
+      <i class="fab fa-google-plus-g"></i>
+    </div>
+  </div>
+  <br>
+  <span style="font-family:courier"><h3 style="text-color:#3aaf9f;"><font face="verdana" color="green"><b>Looking For Flats In Your Area?</b></font></h3><span>
 
 <form method="post" action="display.php">
-    Select Area: <select name="area" class="btn btn-primary dropdown-toggle">
+   <h3> Select Area: </h3><select name="area" class="btn btn-primary dropdown-toggle">
     <option value="-1">Choose</option>
         <option value="Lawgate">Lawgate</option>
         <option value="Phagwara">Phagwara</option>
         <option value="Jalandhar">Jalandhar</option>
 </select>
-<br><br><br>
-Select Flat: <select name="flat" class="btn btn-primary dropdown-toggle">
+<br><br>
+<h3>Select Flat: </h3><select name="flat" class="btn btn-primary dropdown-toggle">
 <option value="-1">Choose</option>
-        <option value="1BHK">1 BHK</option>
-        <option value="2BHK">2 BHK</option>
-        <option value="3BHK">3 BHK</option>
+        <option value="1BHK">1BHK</option>
+        <option value="2BHK">2BHK</option>
+        <option value="3BHK">3BHK</option>
 </select>
-
 <br><br><br>
-<input type="submit" class="btn btn-success" name="submit">
+<input type="submit" class="btn btn-primary" name="submit">
 </form>
 </center>
+
 </body>
     </html>
